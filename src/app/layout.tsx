4fcf8b7ee52b06179683608caf443bb7197c42a3
Lukáš Kubík <lukoluko8@gmail.com>
2025-12-23 12:32:43 +0100

    Fix GA4 inline script using dangerouslySetInnerHTML

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -110,16 +110,19 @@ export default function RootLayout({
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
           strategy="afterInteractive"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_ID}', {
-              page_path: window.location.pathname,
-            });
-          `}
-        </Script>
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              window.gtag = gtag;
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}');
+            `,
+          }}
+        />
         <MotionProvider>
           {children}
         </MotionProvider>
